7d46928cf8475a2325307685171ba75bc1815caa
whibiy <wwwdotkr@gmail.com>
2023-10-31 15:30:55 +0900

    개인 작업 했던 것 합치기

---
+++ src/main/java/ksmart/ks48team02/user/controller/mypage/MypageController.java
@@ -0,0 +1,21 @@
+package ksmart.ks48team02.user.controller.mypage;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user/mypage")
+public class MypageController {
+
+    @GetMapping(value={"","/"})
+    public String dropPage(){
+
+        return "user/mypage/mypage";
+    }
+
+    @GetMapping("profile")
+    public String profilePage(){
+        return "user/mypage/profile";
+    }
+}
