6b6c327b5b4fa2b79057c01731700897d53675dc
jeon-seulji <seulji.1.0.2@gmail.com>
2023-11-03 12:58:53 +0900

    common-custom.js : 유효성 검증 fn 수정 및 추가 fix : 유효성 검증 fn 수정 feat: admin 주문 페이지 추가

---
+++ src/main/java/ksmart/ks48team02/admin/controller/orderManagement/OrderManagement.java
@@ -0,0 +1,53 @@
+package ksmart.ks48team02.admin.controller.orderManagement;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller("adminOrderManagement")
+@RequestMapping("/admin/order")
+public class OrderManagement {
+
+    // 주문 대시보드
+    @GetMapping(value = {"","/"})
+    public String adminOrderDashBoard(Model model){
+        model.addAttribute("title","관리자 : 주문 대시보드");
+        model.addAttribute("contentsTitle","주문 대시보드");
+        model.addAttribute("contentsSubTitle","관리자 주문 대시보드");
+        return "";
+    }
+    // 주문 목록
+    @GetMapping( "/list")
+    public String adminOrderList(Model model){
+        model.addAttribute("title","관리자 : 주문 목록");
+        model.addAttribute("contentsTitle","주문 목록");
+        model.addAttribute("contentsSubTitle","관리자 주문 전체 목록");
+        return "admin/order/list";
+    }
+    // 배송 관리
+    @GetMapping( "/delivery")
+    public String adminOrderDelivery(Model model){
+        model.addAttribute("title","관리자 : 배송 관리");
+        model.addAttribute("contentsTitle","배송 관리");
+        model.addAttribute("contentsSubTitle","관리자 배송 관리");
+        return "admin/order/delivery";
+    }
+    // 교환 환불 신청 관리
+    @GetMapping( "/refundSwapping")
+    public String adminOrderRefundSwapping(Model model){
+        model.addAttribute("title","관리자 : 교환 환불 관리");
+        model.addAttribute("contentsTitle","교환/환불 관리");
+        model.addAttribute("contentsSubTitle","관리자 교환/환불 관리");
+        return "admin/order/refundSwapping";
+    }
+
+    // 주문 확정 목록 조회
+    @GetMapping( "/completedList")
+    public String adminOrderCompletedList(Model model){
+        model.addAttribute("title","관리자 : 주문 확정 목록");
+        model.addAttribute("contentsTitle","주문 확정 목록");
+        model.addAttribute("contentsSubTitle","관리자 주문 확정 목록");
+        return "admin/order/orderCompletedList";
+    }
+}
