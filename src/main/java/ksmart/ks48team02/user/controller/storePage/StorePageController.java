e22205a5409eeca8c5433a2d7ac6fa178abd0897
whibiy <wwwdotkr@gmail.com>
2023-11-08 18:16:17 +0900

    detail : 개인 작업한 것 합치기

---
+++ src/main/java/ksmart/ks48team02/user/controller/storePage/StorePageController.java
@@ -0,0 +1,21 @@
+package ksmart.ks48team02.user.controller.storePage;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller("storePageController")
+@RequestMapping("/user/storePage")
+public class StorePageController {
+    @GetMapping(value = {"","/"})
+    public String storePage(){
+        return "/user/storePage/main";
+    }
+
+    @GetMapping(value = {"/ajax"})
+    @ResponseBody
+    public String storeePage(){
+        return "tab1";
+    }
+}
