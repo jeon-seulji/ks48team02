bfd1607e809ee35f1ec8a1de7431fee7cbcab218
ECS <eaea8@naver.com>
2023-11-08 17:31:27 +0900

    [ksm48team02]김상민 2023-11-08

--- src/main/java/ksmart/ks48team02/user/mapper/reward/RewardMapper.java
+++ src/main/java/ksmart/ks48team02/user/mapper/reward/RewardMapper.java
@@ -1,10 +1,16 @@
 package ksmart.ks48team02.user.mapper.reward;
 
 
+import ksmart.ks48team02.user.dto.reward.RewardCustomerRank;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface RewardMapper {
+
+
+    //회원 등급 별 적립률, 할인율 조회
+    public RewardCustomerRank getRankById(String memberId);
+
 
 
 }
