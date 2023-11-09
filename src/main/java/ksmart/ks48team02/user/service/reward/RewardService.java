0e7b3718aac3a90c704ee18a451271350d1570c4
ECS <eaea8@naver.com>
2023-11-09 14:24:53 +0900

    [ksm48team02]김상민 2023-11-09

--- src/main/java/ksmart/ks48team02/user/service/reward/RewardService.java
+++ src/main/java/ksmart/ks48team02/user/service/reward/RewardService.java
@@ -1,4 +1,27 @@
 package ksmart.ks48team02.user.service.reward;
 
+
+import ksmart.ks48team02.user.dto.Member;
+import ksmart.ks48team02.user.mapper.reward.RewardMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class RewardService {
+
+    //DI 의존성 추가
+    private final RewardMapper rewardMapper;
+
+    public RewardService(RewardMapper rewardMapper){
+        this.rewardMapper =rewardMapper;
+    }
+
+    //주문 회원 정보 조회
+    public Member getOrderMemberInfo (String memberId){
+        Member orderMemberInfo = rewardMapper.getOrderMemberInfo(memberId);
+
+        return orderMemberInfo;
+    }
+
 }
