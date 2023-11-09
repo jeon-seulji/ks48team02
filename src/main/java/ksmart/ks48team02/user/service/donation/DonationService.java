be1ed02059b39adfac48fe2fb1e550370862fbc5
IgNe0us <wjdals1831@naver.com>
2023-11-09 17:35:20 +0900

    [정민 업데이트] 판매자 프로젝트 목록 확인 + 프로젝트 시작 100% 구현 기부 상세 페이지 작업중 [썸네일, 스토리는 작업완료 Aside쪽 작업해야됨]

--- src/main/java/ksmart/ks48team02/user/service/donation/DonationService.java
+++ src/main/java/ksmart/ks48team02/user/service/donation/DonationService.java
@@ -1,21 +1,26 @@
 package ksmart.ks48team02.user.service.donation;
 
+import ksmart.ks48team02.admin.dto.donation.DonationInfo;
 import ksmart.ks48team02.user.dto.donation.DonationRegistration;
-import ksmart.ks48team02.user.mapper.donation.DonationRegistrationMapper;
+import ksmart.ks48team02.user.mapper.donation.DonationUserMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class DonationService{
-    private final DonationRegistrationMapper donationRegistrationMapper;
-    public DonationService(DonationRegistrationMapper donationRegistrationMapper){
-        this.donationRegistrationMapper = donationRegistrationMapper;
+    private final DonationUserMapper donationUserMapper;
+    public DonationService(DonationUserMapper donationUserMapper){
+        this.donationUserMapper = donationUserMapper;
     }
 
     public void addDonation(DonationRegistration donationRegistration){
         System.out.println(donationRegistration);
-        int result = donationRegistrationMapper.addDonation(donationRegistration);
+        int result = donationUserMapper.addDonation(donationRegistration);
+    }
+
+    public DonationInfo getDonationInfo(String donationCode){
+        return donationUserMapper.getDonationInfo(donationCode);
     }
 
 }
