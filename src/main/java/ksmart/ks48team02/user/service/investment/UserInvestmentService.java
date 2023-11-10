8b3f11b2eec7eb10d76c7d7c1b945b3927dfa69a
dnwntjs009 <dnwntjs009@naver.com>
2023-11-10 11:01:42 +0900

    [코드무림] dto 작업

--- src/main/java/ksmart/ks48team02/user/service/investment/UserInvestmentService.java
+++ src/main/java/ksmart/ks48team02/user/service/investment/UserInvestmentService.java
@@ -1,6 +1,6 @@
 package ksmart.ks48team02.user.service.investment;
 
-import ksmart.ks48team02.user.dto.investment.InvestmentRegistration;
+import ksmart.ks48team02.user.dto.InvestmentRegistration;
 import ksmart.ks48team02.user.mapper.investment.InvestmentRegistrationMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
