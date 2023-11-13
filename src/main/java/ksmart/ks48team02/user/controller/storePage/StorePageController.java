7cbd6b29a367798e29084ef6a6addad27844dbf0
whibiy <wwwdotkr@gmail.com>
2023-11-13 09:46:58 +0900

    detail : 개인 작업한 것 합치기

---
+++ src/main/java/ksmart/ks48team02/user/controller/storePage/StorePageController.java
@@ -0,0 +1,31 @@
+package ksmart.ks48team02.user.controller.storePage;
+
+import ksmart.ks48team02.user.dto.StorePageDto;
+import ksmart.ks48team02.user.service.storePage.StorePageService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+import java.util.List;
+
+@Controller("storePageController")
+@RequestMapping("/user/storePage")
+public class StorePageController {
+
+    private final StorePageService storePageService;
+
+    public StorePageController(StorePageService storePageServiceImpl){
+            this.storePageService = storePageServiceImpl;
+        }
+    @GetMapping(value = {"","/"})
+    public String storePage(Model model){
+            List<StorePageDto> storeNewsList = storePageService.storePageNewsList();
+            model.addAttribute("newsList", storeNewsList);
+
+            return "user/storePage/main";
+    }
+
+    }
