ee94555d3cd30424e83a7f48523c677a26c88fa1
whibiy <wwwdotkr@gmail.com>
2023-11-30 12:47:49 +0900

    detail : 개인 작업한 것 합치기

--- src/main/java/ksmart/ks48team02/admin/controller/user/UserManagementController.java
+++ src/main/java/ksmart/ks48team02/admin/controller/user/UserManagementController.java
@@ -5,6 +5,7 @@ import ksmart.ks48team02.admin.service.user.UserManagementService;
 import ksmart.ks48team02.user.dto.Member;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +20,8 @@ public class UserManagementController {
     private final UserManagementService userManagementService;
     @GetMapping("/memberList")
     public String memberListPage(Model model){
+        List<UserManagement> memberList = userManagementService.getMemberList();
+        model.addAttribute("memberList", memberList);
         model.addAttribute("contentsTitle", "회원 목록");
 
         return "admin/user/member_list";
@@ -39,19 +42,25 @@ public class UserManagementController {
         log.info("로그인 로그 {}", loginLogs);
 
         return loginLogs;
-
     }
 
+    // 로그인 내역 조회 페이지
     @GetMapping("/loginLog")
     public String memberLoginLog(Model model){
         // 로그인 로그에서 유저 아이디 작성하고 검색 버튼 클릭시 리스트 반환
-
+        List<UserManagement> loginLogAll = userManagementService.getLoginLog();
+        model.addAttribute("loginLogAll", loginLogAll);
         model.addAttribute("contentsTitle", "회원 로그인 이력");
-        return "/admin/user/login_log";
+        return "admin/user/login_log";
     }
 
+    // 등급 정보 조회 페이지
     @GetMapping("/memberRank")
     public String memberRankPage(Model model){
+        List<UserManagement> rewardRanks = userManagementService.getRewardRank();
+        List<UserManagement>  donationRanks = userManagementService.getDonationRank();
+        model.addAttribute("rewardRanks",rewardRanks);
+        model.addAttribute("donationRanks",donationRanks);
         model.addAttribute("contentsTitle", "프로젝트 유형별 등급 관리");
         return "admin/user/member_rank";
     }
